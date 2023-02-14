4737ce27b968b5e30297f0f94ebb62e95dff5c80
That Blackwolf <107058543+imnagarajan@users.noreply.github.com>
2023-02-14 06:28:57 +0530

    Create ChatGame.cs

---
+++ src/PluginTemplate/ChatGame.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prizes.Models
+{
+    public class ChatGame
+    {
+        public bool Occuring = false;
+        public int answer = 0;
+        public string wordAnswer = "";
+    }
+}
