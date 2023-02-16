de89d2f500a848d41f0f790c47ebd64ac2e360c6
That Blackwolf <107058543+imnagarajan@users.noreply.github.com>
2023-02-16 16:24:06 +0530

    Update AnswerCommand.cs

--- src/PluginTemplate/AnswerCommand.cs
+++ src/PluginTemplate/AnswerCommand.cs
@@ -11,7 +11,7 @@ using TShockAPI;
 
 namespace Prizes.Modules
 {
-    [RequirePermission("aar.user")]
+    [RequirePermission("tbc.user")]
     internal class AnswerCommand : TSModuleBase<TSCommandContext>
     {
         PrizesSettings config = Configuration<PrizesSettings>.Settings;
